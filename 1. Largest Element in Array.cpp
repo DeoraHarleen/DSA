64dbe5fe583b21a0c06435560cb73915d68b2e29
DeoraHarleen <100436671+DeoraHarleen@users.noreply.github.com>
2023-08-25 22:52:15 +0530

    Add files via upload

---
+++ 1. Largest Element in Array.cpp
@@ -0,0 +1,39 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int SortArray(vector <int> &arr)
+// {
+// 	sort(arr.begin(),arr.end());
+// 	return arr[arr.size()-1];
+// }
+// int main()
+// {
+// 	vector<int> arr={2,5,1,3,0};
+// 	cout<<" "<<SortArray(arr);
+// return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+int largestElement(int arr[],int n)
+{
+    int max=arr[0];
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    int arr[]={2,5,1,3,0};
+    int n=5;
+    int max=largestElement(arr,n);
+    cout<<" "<<max;
+}
