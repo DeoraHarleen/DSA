6df7c873d5135fc91f638e6706f8614c620eab04
Harleen Kaur <100436671+DeoraHarleen@users.noreply.github.com>
2024-06-09 16:22:26 +0530

    Create Pattern4.cpp

---
+++ Patterns/Pattern4.cpp
@@ -0,0 +1,19 @@
+/*
+Input: ‘N’ = 3
+
+Output: 
+1
+2 2 
+3 3 3
+*/
+void Pattern4(int n) {
+	// Write your code here
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			cout<<i<<" ";
+		}
+		cout<<endl;
+	}
+}
