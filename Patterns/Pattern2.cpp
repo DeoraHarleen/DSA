905612b0c05c4a60b1da88be6918e2ca1a34a8a3
Harleen Kaur <100436671+DeoraHarleen@users.noreply.github.com>
2024-06-09 16:11:36 +0530

    Create Pattern2.cpp

---
+++ Patterns/Pattern2.cpp
@@ -0,0 +1,19 @@
+/*
+Input:  ‘N’ = 3
+Output: 
+* 
+* *
+* * *
+
+*/
+void Pattern2(int n) {
+	// Write your code here.
+	for(int i=0;i<n;i++)
+	{
+		for (int j=0;j<=i;j++)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
