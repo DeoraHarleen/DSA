ad5fd6572d729967f753d30c50057a66aeca895a
Harleen Kaur <100436671+DeoraHarleen@users.noreply.github.com>
2024-06-09 16:39:39 +0530

    Update Pattern5.cpp

--- Patterns/Pattern5.cpp
+++ Patterns/Pattern5.cpp
@@ -17,3 +17,15 @@ void Pattern5(int n) {
 		cout<<endl;
 	}
 }
+//method 2 using n-i+1
+void Pattern5(int n) {
+	// Write your code here.
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=0;j<n-i+1;j++)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
