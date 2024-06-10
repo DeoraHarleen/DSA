5a3d4b31c359052145813d9553f0aee2fb4252e6
Harleen Kaur <100436671+DeoraHarleen@users.noreply.github.com>
2024-06-10 16:41:41 +0530

    Create Pattern9-nStarDiamond.cpp

---
+++ Patterns/Pattern9-nStarDiamond.cpp
@@ -0,0 +1,48 @@
+/*
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+*****
+ ***
+  *
+
+*/
+void nStarDiamond(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<2*i+1;k++)
+        {
+            cout<<"*"; 
+        }
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<2*n-(2*i+1);k++)
+        {
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
