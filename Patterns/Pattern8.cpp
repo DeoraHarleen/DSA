4d741f24166bf72880124ab4d16683aaf7a4adea
Harleen Kaur <100436671+DeoraHarleen@users.noreply.github.com>
2024-06-10 16:29:40 +0530

    Create Pattern8.cpp

---
+++ Patterns/Pattern8.cpp
@@ -0,0 +1,28 @@
+/*
+Input: ‘N’ = 3
+Output: 
+
+*****
+ ***
+  *
+
+*/
+void Pattern8(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<2*n-(2*i+1);k++)
+        {
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
